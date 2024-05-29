61a3cd9edb332934b14a46a3c09cd6532c4d7e5a
Alan Lima <dev.alanlima@gmail.com>
2024-05-29 13:36:36 -0300

    feat: Create new icon

--- src/components/icons/icons_card_plus.vue
+++ src/components/icons/icons_card_plus.vue
@@ -1,5 +1,1 @@
-<template>
-  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
-    <path d="M2 4h20v16H2V4zm2 2v4h16V6H4zm16 6H10v2h10v-2zm0 4h-4v2h4v-2zm-6 2v-2H4v2h10zM4 14h4v-2H4v2z" fill="currentColor"/> 
-  </svg>
-</template>
+<template> <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M22 4H2v16h10v-2H4V6h16v4h2V4zm-3 13h3v-2h-3v-3h-2v3h-3v2h3v3h2v-3z" fill="currentColor"/> </svg> </template>
